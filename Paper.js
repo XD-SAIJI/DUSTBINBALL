1fe20534835c2b0a0a686361b2f969981e6d2ad3
XD-SAIJI <75777079+XD-SAIJI@users.noreply.github.com>
2021-06-11 14:32:12 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,17 @@
+class Paper
+{
+    constructor(x,y,w,h){
+
+    
+    var options={
+        isStatic:false,
+        restituion:0.3,
+        friction:0,
+        density:1.2
+        
+    }
+
+    this.image=loadImage("paper.png")
+
+    }
+}
